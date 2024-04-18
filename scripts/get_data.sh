a42984871cf0068b628abf01bfe10b0a350375d1
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-04-18 14:51:49 -0300

    exp data analysis

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -4,49 +4,55 @@ if [ ! -d "data/" ]; then
     mkdir data;
 fi
 
+if [ ! -d "data/wyscout" ]; then 
+    mkdir data/wyscout;
+fi
+
 mkdir \
-    data/competitions \
-    data/events \
-    data/matches \
-    data/playeranks \
-    data/teams \
-    data/players \
-    data/referees \
-    data/coaches \
-    data/tagId2name \
-    data/eventId2name \
-    data/minutes_played
+    data/wyscout/competitions \
+    data/wyscout/events \
+    data/wyscout/matches \
+    data/wyscout/playeranks \
+    data/wyscout/teams \
+    data/wyscout/players \
+    data/wyscout/referees \
+    data/wyscout/coaches \
+    data/wyscout/tagId2name \
+    data/wyscout/eventId2name \
+    data/wyscout/minutes_played
 
 # downloads the datasets stored in https://figshare.com/collections/Soccer_match_event_dataset/4415000/5
 # already unzip content and rename the files to be more readable
-wget -P data/competitions https://figshare.com/ndownloader/files/15073685
-mv data/competitions/15073685 data/competitions/competitions.json
+wget -P data/wyscout/competitions https://figshare.com/ndownloader/files/15073685
+mv data/wyscout/competitions/15073685 data/wyscout/competitions/competitions.json
 
-wget -P data/events https://figshare.com/ndownloader/files/14464685
-unzip data/events/14464685 -d data/events/
-rm data/events/14464685
+wget -P data/wyscout/events https://figshare.com/ndownloader/files/14464685
+unzip data/wyscout/events/14464685 -d data/wyscout/events/
+rm data/wyscout/events/14464685
 
-wget -P data/matches https://figshare.com/ndownloader/files/14464622
-unzip data/matches/14464622 -d data/matches/
-rm data/matches/14464622
+wget -P data/wyscout/matches https://figshare.com/ndownloader/files/14464622
+unzip data/wyscout/matches/14464622 -d data/wyscout/matches/
+rm data/wyscout/matches/14464622
 
-wget -P data/playeranks https:/figshare.com/ndownloader/files/16972010
-mv data/playeranks/16972010 data/playeranks/playeranks.json
+wget -P data/wyscout/playeranks https:/figshare.com/ndownloader/files/16972010
+mv data/wyscout/playeranks/16972010 data/wyscout/playeranks/playeranks.json
 
-wget -P data/players https://figshare.com/ndownloader/files/15073721
-mv data/players/15073721 data/players/players.json
+wget -P data/wyscout/players https://figshare.com/ndownloader/files/15073721
+mv data/wyscout/players/15073721 data/wyscout/players/players.json
 
-wget -P data/teams https:/figshare.com/ndownloader/files/15073697
-mv data/teams/15073697 data/teams/teams.json
+wget -P data/wyscout/teams https:/figshare.com/ndownloader/files/15073697
+mv data/wyscout/teams/15073697 data/wyscout/teams/teams.json
 
-wget -P data/referees https:/figshare.com/ndownloader/files/15074030
-mv data/referees/15074030 data/referees/referees.json
+wget -P data/wyscout/referees https:/figshare.com/ndownloader/files/15074030
+mv data/wyscout/referees/15074030 data/wyscout/referees/referees.json
 
-wget -P data/coaches https://figshare.com/ndownloader/files/15073868
-mv data/coaches/15073868 data/coaches/coaches.json
+wget -P data/wyscout/coaches https://figshare.com/ndownloader/files/15073868
+mv data/wyscout/coaches/15073868 data/wyscout/coaches/coaches.json
 
-wget -P data/tagId2name https://figshare.com/ndownloader/files/21385239
-mv data/tagId2name/21385239 data/tagId2name/tagId2name.csv
+wget -P data/wyscout/tagId2name https://figshare.com/ndownloader/files/21385239
+mv data/wyscout/tagId2name/21385239 data/wyscout/tagId2name/tagId2name.csv
 
-wget -P data/eventId2name https://figshare.com/ndownloader/files/21385245
-mv data/eventId2name/21385245 data/eventId2name/eventId2name.csv
+wget -P data/wyscout/eventId2name https://figshare.com/ndownloader/files/21385245
+mv data/wyscout/eventId2name/21385245 data/wyscout/eventId2name/eventId2name.csv
+
+# link for minutes per game data: https://github.com/soccermatics/Soccermatics/tree/main/course/lessons/minutes_played
