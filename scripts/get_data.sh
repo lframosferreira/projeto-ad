36478c72f2b6f8bccd79f71048e0e8051cc41b05
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-06-20 11:59:37 -0300

    complementando report

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -34,16 +34,16 @@ wget -P data/wyscout/matches https://figshare.com/ndownloader/files/14464622
 unzip data/wyscout/matches/14464622 -d data/wyscout/matches/
 rm data/wyscout/matches/14464622
 
-wget -P data/wyscout/playeranks https:/figshare.com/ndownloader/files/16972010
+wget -P data/wyscout/playeranks https://figshare.com/ndownloader/files/16972010
 mv data/wyscout/playeranks/16972010 data/wyscout/playeranks/playeranks.json
 
 wget -P data/wyscout/players https://figshare.com/ndownloader/files/15073721
 mv data/wyscout/players/15073721 data/wyscout/players/players.json
 
-wget -P data/wyscout/teams https:/figshare.com/ndownloader/files/15073697
+wget -P data/wyscout/teams https://figshare.com/ndownloader/files/15073697
 mv data/wyscout/teams/15073697 data/wyscout/teams/teams.json
 
-wget -P data/wyscout/referees https:/figshare.com/ndownloader/files/15074030
+wget -P data/wyscout/referees https://figshare.com/ndownloader/files/15074030
 mv data/wyscout/referees/15074030 data/wyscout/referees/referees.json
 
 wget -P data/wyscout/coaches https://figshare.com/ndownloader/files/15073868
